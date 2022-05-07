cd044aeedb6e8134a298e523d5a665b43d12e298
Rusyda Amalia <rusydalia@mhs.unsyiah.ac.id>
2022-05-07 12:29:19 +0700

    Update background edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -16,6 +16,11 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/style.css"> 
 </head>
+<style>
+  body {
+   background-color: lightgreen;
+  }
+</style>
 <body> 
 <p ALIGN="CENTER">DAFTAR RIWAYAT KESEHATAN</p>
 <BR>
