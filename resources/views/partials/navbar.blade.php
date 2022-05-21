f6d9a070ef8ca0e81f7c773e5e321e7a42df8bf8
Khairul Umam ALbi <khairul_um@mhs.unsyiah.ac.id>
2022-05-22 04:11:01 +0700

    memperbaiki tampilan

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,35 @@
+    <!-- navbar -->
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light.text-$green-500 bg-light shadow-sm ">
+            <div class="container">
+                <a class="navbar-brand" href="#">Riwayat Kesehatan Pasien</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav ms-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="/beranda">Beranda</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/data_pasien">Layanan</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/profil">Profil</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/panduanlayanan">Panduan Layanan</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Log out</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <!-- Navbar akhir -->
